af8feaab2da04ea4cb875d99fd2a6dd835f387bc
JDjakubd <162459443+JDjakubd@users.noreply.github.com>
2024-03-20 13:26:07 +0000

    2.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String str = "Hello World!";
+    char[] chars = str.toCharArray();
+    for (char c : chars)
+      {System.out.println(c);}
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
